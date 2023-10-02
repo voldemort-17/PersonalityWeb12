fbdbe7304e83477f167cee391280be452a2c5610
Yash Sharma <129134791+voldemort-17@users.noreply.github.com>
2023-10-02 12:53:49 +0530

    Add files via upload

---
+++ personality prediction/c.js
@@ -0,0 +1,129 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "Do you have social behaviour?",
+    answer: "Very socialising",
+    options: [
+      "fail to express myself",
+      "Just with my peers",
+      "Yes, Very socialising",
+      "Not much"
+    ]
+  },
+    {
+    numb: 2,
+    question: "Do you help people if you are in hurry?",
+    answer: "Surely",
+    options: [
+      "Surely",
+      "Allow others to do",
+      "Will do if i have time",
+      "Feel shy to do so"
+    ]
+  },
+    {
+    numb: 3,
+    question: "Do you love to play outdoor games ?",
+    answer: "Love to play",
+    options: [
+      "Love to play",
+      "Not much",
+      "Only with friends",
+      "Depends on mood"
+    ]
+  },
+    {
+    numb: 4,
+    question: "Do you have qualities of leadership?",
+    answer: "Yes",
+    options: [
+      "Fail to express",
+      "Nope",
+      "I am a follower",
+      "Yes"
+    ]
+  },
+    {
+    numb: 5,
+    question: "Do you stay quiet and shy ?",
+    answer: "No at all",
+    options: [
+      "Yes",
+      "Little bit",
+      "Not at all",
+      "in frony of many people"
+    ]
+  },
+    {
+    numb: 6,
+    question: "Do you like playing sports ?",
+    answer: "Yes, a lot",
+    options: [
+      "Yes, a lot",
+      "Little bit",
+      "Not at all",
+      "Weekends"
+    ]
+  },
+    {
+    numb: 7,
+    question: "What do you do to remain calm under pressure ?",
+    answer: "Ask for help",
+    options: [
+      "Ask for help",
+      "Cry",
+      "Regret your own actions",
+      "Take a break"
+    ]
+  },
+    {
+    numb: 8,
+    question: "Do you find it easy to make new friends ?",
+    answer: "Yes",
+    options: [
+      "Yes",
+      "Only if our vibes match",
+      "Not at all, feel shy to talk",
+      "Not interested in friendship"
+    ]
+  },
+    {
+    numb: 9,
+    question: "Do you make backup plan for any plan ?",
+    answer: "Yes",
+    options: [
+      "Yes",
+      "Backup plans are waste",
+      "Naah",
+      "Work hard for only one plan"
+    ]
+  },
+    {
+    numb: 10,
+    question: "Do you complete one task before starting another ?",
+    answer: "Depends on the priority of task",
+    options: [
+      "Yes",
+      "Nope, leave tasks incomplete",
+      "Depends on the priority of task",
+      "No time for any tasks"
+    ]
+  },
+  
+  
+  // you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+  //   {
+  //   numb: 6,
+  //   question: "Your Question is Here",
+  //   answer: "Correct answer of the question is here",
+  //   options: [
+  //     "Option 1",
+  //     "option 2",
+  //     "option 3",
+  //     "option 4"
+  //   ]
+  // },
+];
